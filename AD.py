348041126b8300c13fd23be518ebf445a400ceb0
Hualex <du.xiaolong.chn@gmail.com>
2019-06-10 22:22:19 +0200

    add new AD model with feature extration

--- AD.py
+++ AD.py
@@ -1,7 +1,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as im
 import skimage
+import torch.nn as nn
 
 from scipy.ndimage.filters import convolve
 from skimage.data import binary_blobs, chelsea
@@ -12,64 +14,7 @@ from skimage.color import rgb2gray, label2rgb
 from skimage.restoration import estimate_sigma
 
 
-class PM_AD():
-    def __init__(self, img, T):
-        self.image = img
-        self.T = T
-
-    def g(self, gradient, mode=1, k=60):
-        if mode == 4:
-            dk_term = np.abs(gradient)/(2.*k)
-            dk_term[dk_term > 1] = 1
-        else:
-            dk_term = np.power(np.abs(gradient)/k, 2)
-        result = np.zeros_like(gradient)
-        if mode == 1:
-            result = np.exp(-1*dk_term)
-        elif mode == 2:
-            result = 1/(1+dk_term)
-        elif mode == 3:
-            result = 1/np.sqrt(1+dk_term)
-        elif mode == 4:
-            result = np.power(1-np.power(dk_term, 2), 2)
-        return result
-
-    def dmaps(self, data):
-        d0_filter = np.array([[0, 0, 0], [0, -1, 1], [0, 0, 0]])
-        d0 = convolve(data, d0_filter, mode='reflect')
-        d1_filter = np.array([[0, 0, 0], [0, -1, 0], [0, 1, 0]])
-        d1 = convolve(data, d1_filter, mode='reflect')
-        d2_filter = np.array([[0, 0, 0], [1, -1, 0], [0, 0, 0]])
-        d2 = convolve(data, d2_filter, mode='reflect')
-        d3_filter = np.array([[0, 1, 0], [0, -1, 0], [0, 0, 0]])
-        d3 = convolve(data, d3_filter, mode='reflect')
-        return (d0, d1, d2, d3)
-
-    def update_image(self, I, dmap, alpha=0.2, mode=1, k=50):
-        """
-        Principles_Of_Digital_ImageProces_Advanced
-        DOI 10.1007/978-1-84882-919-0
-        """
-        delta_I = np.zeros_like(I, dtype=float)
-        for d in dmap:
-            est_k = self.estimate_k(d)
-            delta_I += alpha*self.g(d, mode=mode, k=est_k)*d
-        return I+delta_I
-
-    def estimate_k(self, dmap):
-        k = np.median(np.abs(dmap-np.abs(np.median(dmap))))
-        # print(k)
-        return k
-
-    def apply_diffusion(self):
-        I = self.image
-        T = self.T
-        for _ in range(T):
-            dmaps = self.dmaps(I)
-            I = self.update_image(I, dmaps)
-        return I
-
-
+"""
 class TD_AD():
     def __init__(self, img, T=10):
         self.img = img
@@ -200,15 +145,82 @@ class TRND(nn.Module):
             in_x = in_x-self.diffusion(in_x, diff_layer)
         y = in_x
         return y
+"""
+
+
+class PM_AD():
+    def __init__(self, img, T):
+        self.image = img
+        self.T = T
+
+    def g(self, gradient, mode=1, k=60):
+        if mode == 4:
+            dk_term = np.abs(gradient)/(2.*k)
+            dk_term[dk_term > 1] = 1
+        else:
+            dk_term = np.power(np.abs(gradient)/k, 2)
+        result = np.zeros_like(gradient)
+        if mode == 1:
+            result = np.exp(-1*dk_term)
+        elif mode == 2:
+            result = 1/(1+dk_term)
+        elif mode == 3:
+            result = 1/np.sqrt(1+dk_term)
+        elif mode == 4:
+            result = np.power(1-np.power(dk_term, 2), 2)
+        return result
+
+    def dmaps(self, data):
+        d0_filter = np.array([[0, 0, 0], [0, -1, 1], [0, 0, 0]])
+        d0 = convolve(data, d0_filter, mode='reflect')
+        d1_filter = np.array([[0, 0, 0], [0, -1, 0], [0, 1, 0]])
+        d1 = convolve(data, d1_filter, mode='reflect')
+        d2_filter = np.array([[0, 0, 0], [1, -1, 0], [0, 0, 0]])
+        d2 = convolve(data, d2_filter, mode='reflect')
+        d3_filter = np.array([[0, 1, 0], [0, -1, 0], [0, 0, 0]])
+        d3 = convolve(data, d3_filter, mode='reflect')
+        return (d0, d1, d2, d3)
+
+    def update_image(self, I, dmap, alpha=0.2, mode=1, k=50):
+        """
+        Principles_Of_Digital_ImageProces_Advanced
+        DOI 10.1007/978-1-84882-919-0
+        """
+        delta_I = np.zeros_like(I, dtype=float)
+        for d in dmap:
+            est_k = self.estimate_k(d)
+            # print(est_k)
+            delta_I += alpha*self.g(d, mode=mode, k=0.2)*d
+        return I+delta_I
+
+    def estimate_k(self, dmap):
+        k = np.median(np.abs(dmap-np.abs(np.median(dmap))))
+        # print(k)
+        return k
+
+    def apply_diffusion(self):
+        I = self.image
+        T = self.T
+        for _ in range(T):
+            dmaps = self.dmaps(I)
+            I = self.update_image(I, dmaps)
+        return I
 
 
 if __name__ == "__main__":
+    from PIL import Image
     data_orig = skimage.img_as_float(binary_blobs(length=128, seed=1))
     sigma = 0.2
     data = random_noise(data_orig, var=sigma**2)
     I = data
-    diffusion_denoising = TD_AD(I)
-    I = diffusion_denoising.apply_diffusion()
+
+    diffusion_denoising = PM_AD(I, 100)
+    I_d = diffusion_denoising.apply_diffusion()
+    #print(compare_mse(data_orig, I), compare_mse(data_orig, I_d))
+    #print(compare_ssim(data_orig, I), compare_ssim(data_orig, I_d))
+    im.imsave('clean_I.jpeg', data_orig)
+    im.imsave('noisy_I.jpeg', data)
+    im.imsave('denoised_I.jpeg', I_d)
 
     plt.figure(figsize=(20, 20))
     plt.subplot(131)
@@ -216,5 +228,5 @@ if __name__ == "__main__":
     plt.subplot(132)
     plt.imshow(data, cmap='gray')
     plt.subplot(133)
-    plt.imshow(I, cmap='gray')
+    plt.imshow(I_d, cmap='gray')
     plt.show()
--- testAD.py
+++ testAD.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage
-
+import torch
 from scipy.ndimage.filters import convolve
 from skimage.data import binary_blobs, chelsea
 from skimage.exposure import rescale_intensity
@@ -149,6 +149,65 @@ def loss_with_mse_and_cross(I_out, I, label_out, out_resnet, alpha=0.1):
     loss_c = torch.nn.functional.cross_entropy(out_resnet, label)
     loss_r = torch.nn.functional.mse_loss(I_out, I)
     return loss_r+alpha*loss_c
+
+
+class AD_F(nn.Module):
+    def __init__(self, k=4, T=3, image_channels=1, kernel_size=3):
+        super(TRND, self).__init__()
+        self.kernel_size = kernel_size
+        padding = int((kernel_size-1)/2)
+        self.k = k
+        self.T = T
+        self.padding = padding
+        self.diff_layers = nn.ModuleList([nn.Conv2d(
+            in_channels=4, out_channels=k, kernel_size=kernel_size, padding=padding, bias=True) for i in range(T)])
+
+    def g(self, d_I, mode=1):
+        if mode == 1:
+            exponent = -torch.abs(d_I)/(1+d_I**2)
+            res = torch.exp(exponent)*d1
+        elif mode == 2:
+            res = torch.reciprocal(1+d_I**2)*d_I
+        elif mode == 3:
+            res = torch.sqrt(1+d_I**2)*d_I
+        elif mode == 4:
+            res = torch.exp(-1*d_I**2)
+        return res
+
+    def squash_tensor(self, x):
+        n, c, h, w = x.size()
+        x_squashed = torch.zeros(n, 1, h, w).cuda()
+        for i in range(self.k):
+            f = x[:, i, :, :]
+            f.unsqueeze_(1)
+            x_squashed = x_squashed+f
+        return x_squashed
+
+    def extract_feature(self, x):
+        n, c, h, w = x.size()
+        features = torch.zeros(n, 4, h, w).cuda()
+        conv_filter_1 = torch.Tensor([[[[0, 1, 0]], [[0, 0, 0]], [[0, 0, 0]]]])
+        conv_filter_2 = torch.Tensor([[[[0, 0, 0]], [[0, 0, 0]], [[0, 1, 0]]]])
+        conv_filter_3 = torch.Tensor([[[[0, 0, 0]], [[1, 0, 0]], [[0, 0, 0]]]])
+        conv_filter_4 = torch.Tensor([[[[0, 0, 0]], [[0, 0, 1]], [[0, 0, 0]]]])
+        feature_filters = [conv_filter_1,
+                           conv_filter_2, conv_filter_3, conv_filter_4]
+        for i, filter in enumerate(feature_filters):
+            filter.transpose_(2, 1)
+            f = torch.nn.functional.conv2d(x, filter.cuda(), padding=1)
+            t = f.squeeze(1)
+            features[:, i, :, :] = t
+        return features
+
+    def forward(self, x):
+        in_x = x
+        for diff_layer in self.diff_layers:
+            feature_x = self.extract_feature(in_x)
+            d1 = diff_layer(feature_x)
+            features = self.g(d1, mode=4)*d1
+            in_x = in_x - self.squash_tensor(features)/self.k
+        y = in_x
+        return y
 
 
 if __name__ == "__main__":
