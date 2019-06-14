05423c5a2df5888b0b502f75319ae1cd3e2f956b
Hualex <du.xiaolong.chn@gmail.com>
2019-06-14 17:15:01 +0200

    Deep AD and Deep AD F model

--- AD.py
+++ AD.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as im
@@ -14,7 +13,6 @@ from skimage.color import rgb2gray, label2rgb
 from skimage.restoration import estimate_sigma
 
 
-"""
 class TD_AD():
     def __init__(self, img, T=10):
         self.img = img
@@ -104,49 +102,63 @@ class TD_AD():
             I = self.update_image(I, alpha, beta_k)
         return I
 
-
-class TRND(nn.Module):
+class Deep_AD_F(nn.Module):
     def __init__(self, k=4, T=3, image_channels=1, kernel_size=3):
-        super(TRND, self).__init__()
-        kernel_size = 3
-        padding = 1
+        super(Deep_AD_F, self).__init__()
+        self.kernel_size = kernel_size
+        padding = int((kernel_size-1)/2)
         self.k = k
         self.T = T
+        self.padding = padding
         self.diff_layers = nn.ModuleList([nn.Conv2d(
-            in_channels=k, out_channels=k, kernel_size=kernel_size, padding=padding, bias=False, groups=k) for i in range(T)])
+            in_channels=4, out_channels=k, kernel_size=kernel_size, padding=padding, bias=True) for i in range(T)])
 
     def g(self, d_I, mode=1):
         if mode == 1:
             exponent = -torch.abs(d_I)/(1+d_I**2)
-            res = torch.exp(exponent)*d1
+            res = torch.exp(exponent)
         elif mode == 2:
-            res = torch.reciprocal(1+d_I**2)*d_I
+            res = torch.reciprocal(1+d_I**2)
         elif mode == 3:
-            res = torch.sqrt(1+d_I**2)*d_I
+            res = torch.sqrt(1+d_I**2)
         elif mode == 4:
             res = torch.exp(-1*d_I**2)
         return res
 
-    def diffusion(self, x, diff_layer, mode=4):
+    def squash_tensor(self, x):
         n, c, h, w = x.size()
-        input_x = torch.zeros(n, self.k, h, w).cuda()+x
-        sum_delta = torch.zeros(n, 1, h, w).cuda()
-        d = diff_layer(input_x)
-        delta_d = self.g(d, mode=mode)
+        x_squashed = torch.zeros(n, 1, h, w).cuda()
         for i in range(self.k):
-            f = delta_d[:, i, :, :]
+            f = x[:, i, :, :]
             f.unsqueeze_(1)
-            sum_delta = sum_delta + f
-        return sum_delta/self.k
+            x_squashed = x_squashed+f
+        return x_squashed
+
+    def extract_feature(self, x):
+        n, c, h, w = x.size()
+        features = torch.zeros(n, 4, h, w).cuda()
+        conv_filter_1 = torch.Tensor([[[[0, 1, 0]], [[0, 0, 0]], [[0, 0, 0]]]])
+        conv_filter_2 = torch.Tensor([[[[0, 0, 0]], [[0, 0, 0]], [[0, 1, 0]]]])
+        conv_filter_3 = torch.Tensor([[[[0, 0, 0]], [[1, 0, 0]], [[0, 0, 0]]]])
+        conv_filter_4 = torch.Tensor([[[[0, 0, 0]], [[0, 0, 1]], [[0, 0, 0]]]])
+        feature_filters = [conv_filter_1,
+                           conv_filter_2, conv_filter_3, conv_filter_4]
+        for i, filter in enumerate(feature_filters):
+            filter.transpose_(2, 1)
+            f = torch.nn.functional.conv2d(x, filter.cuda(), padding=1)
+            t = f.squeeze(1)
+            features[:, i, :, :] = t
+        return features
 
     def forward(self, x):
         in_x = x
         for diff_layer in self.diff_layers:
-            in_x = in_x-self.diffusion(in_x, diff_layer)
+            feature_x = self.extract_feature(in_x)
+            d1 = diff_layer(feature_x)
+            features = self.g(d1, mode=4)*d1
+            in_x = in_x - self.squash_tensor(features)/self.k
         y = in_x
         return y
-"""
-
 
 class PM_AD():
     def __init__(self, img, T):
@@ -206,6 +218,45 @@ class PM_AD():
             I = self.update_image(I, dmaps)
         return I
 
+class Deep_AD(nn.Module):
+    def __init__(self, k=4, T=3, image_channels=1, kernel_size=3):
+        super(Deep_AD, self).__init__()
+        self.kernel_size = kernel_size
+        padding = int((kernel_size-1)/2)
+        self.k = k
+        self.T = T
+        self.diff_layers = nn.ModuleList([nn.Conv2d(
+            in_channels=1, out_channels=k, kernel_size=kernel_size, padding=padding, bias=True) for i in range(T)])
+
+    def g(self, d_I, mode=1):
+        if mode == 1:
+            exponent = -torch.abs(d_I)/(1+d_I**2)
+            res = torch.exp(exponent)
+        elif mode == 2:
+            res = torch.reciprocal(1+d_I**2)
+        elif mode == 3:
+            res = torch.sqrt(1+d_I**2)
+        elif mode == 4:
+            res = torch.exp(-1*d_I**2)
+        return res
+
+    def squash_tensor(self, x):
+        n, c, h, w = x.size()
+        x_squashed = torch.zeros(n, 1, h, w).cuda()
+        for i in range(self.k):
+            f = x[:, i, :, :]
+            f.unsqueeze_(1)
+            x_squashed = x_squashed+f
+        return x_squashed
+
+    def forward(self, x):
+        in_x = x
+        for diff_layer in self.diff_layers:
+            d1 = diff_layer(in_x)
+            features = self.g(d1, mode=1)*d1
+            in_x = in_x - self.squash_tensor(features)/self.k
+        y = in_x
+        return y
 
 if __name__ == "__main__":
     from PIL import Image
